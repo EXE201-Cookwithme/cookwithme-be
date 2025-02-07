9e2b1631a5cec7d23c1a0960aedc0a48783945a3
thangngo <ngosythang123456@gmail.com>
2025-02-07 16:15:29 +0700

    feat: update main ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MongoExceptionFilter } from './exception-filters/mongo-exception.filter';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new MongoExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(8080);
 }
 bootstrap();
