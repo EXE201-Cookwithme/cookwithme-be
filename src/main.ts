b9447366f60e87d92f301a81e8c7c0af1238f9aa
thangngo <ngosythang123456@gmail.com>
2025-02-08 08:28:06 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   const config = new DocumentBuilder()
     .setTitle('Cookwithme Project')
     .setDescription('The Cookwithme API description')
